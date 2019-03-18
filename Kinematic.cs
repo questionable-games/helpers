d584e36a69fb56f1be4bed9553ad78e7c1503c4c
Alberto Pérez García-Plaza <alpgarcia@gmail.com>
2019-03-19 00:50:27 +0100

    Add helper methods to shoot an object towards another
    Includes one method to calculate shoot velocity and another one
    to draw the path source object is going to follow (in debug mode).

--- Kinematic.cs
+++ Kinematic.cs
@@ -39,4 +39,79 @@ public class Kinematic
         return (-coeff_b + Mathf.Sqrt(coeff_b * coeff_b - 4 * coeff_a * coeff_c))
                  / (2 * coeff_a);
     }
+
+    /// <summary>
+    /// Calculates the params needed to shoot the source object to the
+    /// target one.
+    /// </summary>
+    /// <returns><code>ShootData</code> object containing Initial velocity and 
+    /// Time to target.</returns>
+    /// <param name="source">Source.</param>
+    /// <param name="target">Target.</param>
+    /// <param name="gravity">Gravity.</param>
+    /// <param name="delta_h">The maximum height of the shoot starting from
+    /// target y postion.</param>
+    public static ShootData CalculateShoot(Transform source, Transform target,
+                                           float gravity, float delta_h)
+    {
+        // Calculate highest vertical point of the shoot
+        // Must be greater than target.position.y
+        float h = target.position.y + delta_h;
+
+        float displacementX = target.position.x - source.position.x;
+        float displacementY = target.position.y - source.position.y;
+        float displacementZ = target.position.z - source.position.z;
+
+        float timeToTarget = Mathf.Sqrt(-2 * h / gravity) +
+                     Mathf.Sqrt(2 * (displacementY - h) / gravity);
+
+        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * h);
+
+        // Calculate X and Z together as the equation is the same for both
+        Vector3 displacementXZ = new Vector3(displacementX,
+                                             0,
+                                             displacementZ);
+        Vector3 velocityXZ = displacementXZ / timeToTarget;
+
+        // Sum vectors to get the three components together into a single vector
+        // make sure Y velocity is always against the gravity 
+        Vector3 initialVelocity = velocityXZ + velocityY * -Mathf.Sign(gravity);
+
+        return new ShootData(initialVelocity, timeToTarget);
+    }
+
+    /// <summary>
+    /// Draws the path of the shoot from source object current position to
+    /// target object current position.
+    /// </summary>
+    /// <param name="source">Source.</param>
+    /// <param name="target">Target.</param>
+    /// <param name="gravity">Gravity.</param>
+    /// <param name="delta_h">The maximum height of the shoot starting from
+    /// target y postion.</param>
+    public static void DrawPath(Transform source, Transform target, 
+                                float gravity, float delta_h)
+    {
+        ShootData shootData = Kinematic.CalculateShoot(source,
+                                                       target,
+                                                       gravity,
+                                                       delta_h);
+        Vector3 previousDrawPoint = source.position;
+
+        int resolution = 30;
+        for (int i = 1; i <= resolution; i++)
+        {
+            float simulationTime = (i / (float)resolution) * shootData.timeToTarget;
+            Vector3 displacement = shootData.initialVelocity * simulationTime
+                                   + Vector3.up * gravity
+                                   * simulationTime * simulationTime /
+                                   2f;
+            Vector3 drawPoint = source.position + displacement;
+
+            // TODO Find a way to draw trajectory not only in debug mode (to be
+            //      able to visualizing it in the actual scene, not only in Scene tab)
+            Debug.DrawLine(previousDrawPoint, drawPoint, Color.green);
+            previousDrawPoint = drawPoint;
+        }
+    }
 }
---
+++ ShootData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public struct ShootData
+{
+    public readonly Vector3 initialVelocity;
+    public readonly float timeToTarget;
+
+    public ShootData(Vector3 initialVelocity, float timeToTarget)
+    {
+        this.initialVelocity = initialVelocity;
+        this.timeToTarget = timeToTarget;
+    }
+}
